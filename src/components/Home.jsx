44bcea9b31d91c3e325b36832a61df1caf38d2f1
Uday1004 <Udaysolanki530@gmail.com>
2023-12-19 14:25:24 +0530

    hh

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import MainSlider from './Slider/MainSlider'
 import MenuView from './Products/MenuView'
+import { MdFeedback } from "react-icons/md";
+import "../components/Home.css";
 
 export default function Home() {
   return (
     <div style={{ marginTop: '4rem' }}>
+      <div className="col" style={{width:'95%'}}>
+         <div className='text-right'>
+          <button className='btn btn-success fixed shake ' title='feedback!' > <div title='feedback!'>  <MdFeedback /></div> </button>
+         </div>
+
+      </div>  
         <MainSlider/>
         <MenuView/>
     </div>
